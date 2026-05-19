114832a5debf24873ce8c5f1c5d36617b78ac873
Syed Usman <imsyedusman@gmail.com>
2026-05-19 17:20:04 +1000

    fix: auth

--- src/auth.ts
+++ src/auth.ts
@@ -36,6 +36,7 @@ declare module "next-auth/jwt" {
 }
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
+  trustHost: true,
   providers: [
     Credentials({
       name: "Credentials",
