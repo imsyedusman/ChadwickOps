132bb5a525fa3d0bf7a4e654f090672302f85ada
Syed Usman <imsyedusman@gmail.com>
2026-06-03 09:31:17 +1000

    style: increase width of the wip table

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -24,7 +24,7 @@ export default async function DashboardLayout({
           <div className="flex flex-col h-full overflow-hidden relative border-l border-slate-200/60 dark:border-slate-800/60">
             <Header user={{ name: session.user.name || "User", email: session.user.email || "" }} />
             <main className="flex-1 overflow-y-auto scroll-pt-16">
-              <div className="max-w-[1600px] mx-auto p-6 md:p-8 lg:p-10">
+              <div className="max-w-[1900px] mx-auto p-6 md:p-8 lg:p-10">
                 {children}
               </div>
             </main>
